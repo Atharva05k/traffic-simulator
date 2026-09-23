4edfecbd4b1e3530ef285f37eaec15b2cdb0a6ef
Atharva <atharvakushare05@gmail.com>
2026-09-23 10:39:38 +0530

    Add vehicle and lane domain models

---
+++ traffic_simulator/models.py
@@ -0,0 +1,41 @@
+from collections import deque
+from dataclasses import dataclass, field
+from enum import Enum
+
+class Direction(Enum):
+    NORTH = "NORTH"
+    EAST = "EAST"
+    SOUTH = "SOUTH"
+    WEST = "WEST"
+
+@dataclass
+class Vehicle:
+    vehicle_id: int
+    direction: Direction
+    spawn_time: float
+
+@dataclass
+class Lane:
+    direction: Direction
+
+    vehicles: deque[Vehicle] = field(
+        default_factory=deque
+    )
+
+    def add_vehicle(self, vehicle:Vehicle):
+        self.vehicles.append(vehicle)
+
+    def remove_vehicle(self):
+        if self.vehicles:
+            return self.vehicles.popleft()
+
+        return None
+
+    def next_vehicle(self):
+        if self.vehicles:
+            return self.vehicles[0]
+
+        return None
+
+    def vehicle_count(self):
+        return len(self.vehicles)
